9c95249ce55134ac57fc41815853f684e48e2ea6
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2022-12-10 00:18:51 +0100

    refactor(modules): refactor QuadLU based on functional version and introduce UncertainQuadLU draft

--- src/gum_compliant_neural_network_uncertainty_propagation/modules.py
+++ src/gum_compliant_neural_network_uncertainty_propagation/modules.py
@@ -1,13 +1,17 @@
-"""Contains the custom activation function QuadLU for now"""
+"""Contains the custom activation functions based on QuadLU for now"""
 
-__all__ = ["QuadLU"]
+__all__ = ["QuadLU", "QUADLU_ALPHA_DEFAULT", "UncertainQuadLU"]
 
-from typing import cast, Optional
+from typing import Optional
 
-import torch
 from torch import Tensor
 from torch.nn import Module
 from torch.nn.parameter import Parameter
+
+from gum_compliant_neural_network_uncertainty_propagation.functionals import (
+    quadlu,
+    QUADLU_ALPHA_DEFAULT,
+)
 
 
 class QuadLU(Module):
@@ -33,21 +37,64 @@ class QuadLU(Module):
             trainable, non-negative parameter of QuadLU activation function
         """
 
-    ALPHA_DEFAULT: float = 1.0
+    QUADLU_ALPHA_DEFAULT: Parameter = QUADLU_ALPHA_DEFAULT
 
-    def __init__(self, alpha: Optional[float] = None):
+    def __init__(self, alpha: Optional[Parameter] = None):
         """QuadLU activation function with parameter alpha"""
         super().__init__()
         if alpha is None:
-            self._alpha = Parameter(torch.tensor(self.ALPHA_DEFAULT))
+            self._alpha = self.QUADLU_ALPHA_DEFAULT
         else:
-            self._alpha = Parameter(torch.tensor(alpha))
-        self._alpha.requires_grad = True
+            self._alpha = alpha
 
-    def forward(self, x_in: Tensor) -> Tensor:
+    def forward(self, values: Tensor) -> Tensor:
         """Forward pass of QuadLU"""
-        if x_in <= -self._alpha:
-            return torch.tensor(0.0)
-        if x_in >= self._alpha:
-            return 4.0 * self._alpha * x_in
-        return cast(Tensor, (x_in + self._alpha) ** 2)
+        return quadlu(values, self._alpha)
+
+
+class UncertainQuadLU(Module):
+    r"""Implementation of parametrized QuadLU activation with uncertainty propagation
+
+        :math:`\operatorname{QuadLU}_\alpha \colon \mathbb{R} \to \mathbb{R}` is defined
+        as
+
+        .. math::
+
+            \operatorname{QuadLU}_\alpha (x) :=
+            \begin{cases}
+              0, &\quad \text{for } x \leq -\alpha \\
+              (x + \alpha)^2, &\quad \text{for } -\alpha < x < \alpha \\
+              4\alpha x, &\quad \text{for } x \geq \alpha \\
+            \end{cases}
+
+        with :math:`\alpha \in \mathbb{R}_+`. The uncertainty propagation is performed
+        as stated in the thesis
+
+        .. math::
+
+            \mathbf{U}_{x^{(\ell)}} = \prod_{i=0}^{\ell-1} \operatorname{diag}
+            \frac{\mathrm{d} \operatorname{QuadLU}}{\mathrm{d} z} (z^{(\ell-i)})
+            W^{(\ell-i)} \mathbf{U}_{x^{(0)}} \prod_{i=1}^{\ell} {W^{(i)}}^T
+            \operatorname{diag} \frac{\mathrm{d} \operatorname{QuadLU}}{\mathrm{d} z}
+            (z^{(i)}),
+
+        Parameters
+        ----------
+        alpha : float
+            trainable, non-negative parameter of QuadLU activation function
+        """
+
+    QUADLU_ALPHA_DEFAULT: Parameter = QUADLU_ALPHA_DEFAULT
+
+    def __init__(self, alpha: Optional[Parameter] = None):
+        """Parametrized QuadLU activation function and uncertainty propagation"""
+        super().__init__()
+        self._quadlu = QuadLU(alpha)
+        self._alpha = self._quadlu._alpha
+
+    def forward(
+        self, values: Tensor, uncertainties: Optional[Tensor] = None
+    ) -> tuple[Tensor, Optional[Tensor]]:
+        """Forward pass of UncertainQuadLU"""
+        values = self._quadlu.forward(values)
+        return values, uncertainties
