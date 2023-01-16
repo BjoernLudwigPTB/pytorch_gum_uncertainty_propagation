adcc4ec388424ac386758b4cfb4a22cacb81d193
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2023-01-16 21:24:21 +0100

    feat(propagate): introduce current version of zema_emc_annotated and thus more flexibility

--- src/pytorch_gum_uncertainty_propagation/examples/propagate.py
+++ src/pytorch_gum_uncertainty_propagation/examples/propagate.py
@@ -7,10 +7,13 @@ from math import ceil
 from typing import Any, Type
 
 import torch
-from torch import Tensor
 from torch.autograd.profiler import profile
 from torch.nn import Module
+from zema_emc_annotated.dataset import ZeMASamples  # type: ignore[import]
 
+from pytorch_gum_uncertainty_propagation.examples.zema_dataset import (
+    convert_zema_std_uncertainties_into_synthetic_full_cov_matrices,
+)
 from pytorch_gum_uncertainty_propagation.modules import (
     GUMQuadLUMLP,
     GUMSigmoidMLP,
@@ -20,47 +23,47 @@ from pytorch_gum_uncertainty_propagation.modules import (
 from pytorch_gum_uncertainty_propagation.uncertainties import (
     UncertainTensor,
 )
-from pytorch_gum_uncertainty_propagation.zema_dataset import (
-    convert_zema_std_uncertainties_into_synthetic_full_cov_matrices,
-    provide_zema_samples,
-)
 
 
 def assemble_pipeline(
     generic_mlp_module: Type[Module],
-    n_samples: int = 1,
-    input_values: UncertainTensor | Tensor | None = None,
+    size_scaler: int = 1,
+    idx_start: int = 0,
+    out_features: int = 2,
+    depth: int = 1,
 ) -> Any:
     """Propagate data through an MLP equipped with GUMQuadLU activation"""
     torch.set_default_dtype(torch.double)  # type: ignore[no-untyped-call]
-    if input_values is None:
-        if "GUM" in generic_mlp_module.__name__:
-            input_values = (
-                convert_zema_std_uncertainties_into_synthetic_full_cov_matrices(
-                    n_samples
-                )
-            )
-            assert input_values.uncertainties is not None
-            mlp = generic_mlp_module(
-                input_values.values.shape[1],
-                _construct_partition(input_values.values.shape[1]),
-            )  # type: ignore[call-arg]
-            for uncertain_value in zip(input_values.values, input_values.uncertainties):
-                with profile(
-                    with_stack=True
-                ) as profiler:  # type: ignore[no-untyped-call]
-                    mlp(UncertainTensor(*uncertain_value))
-        else:
-            input_values = provide_zema_samples(n_samples).values
-            mlp = generic_mlp_module(
-                input_values.shape[1], _construct_partition(input_values.shape[1])
-            )  # type: ignore[call-arg]
-            for value in input_values:
-                with profile(
-                    with_stack=True
-                ) as profiler:  # type: ignore[no-untyped-call]
-                    mlp(value)
-
+    if "GUM" in generic_mlp_module.__name__:
+        input_values = convert_zema_std_uncertainties_into_synthetic_full_cov_matrices(
+            n_samples=1, size_scaler=size_scaler, normalize=True, idx_start=idx_start
+        )
+        assert input_values.uncertainties is not None
+        mlp = generic_mlp_module(
+            input_values.values.shape[1],
+            _construct_out_features_counts(
+                input_values.values.shape[1], out_features, depth
+            ),
+        )  # type: ignore[call-arg]
+        for uncertain_value in zip(input_values.values, input_values.uncertainties):
+            with profile(with_stack=True) as profiler:  # type: ignore[no-untyped-call]
+                mlp(UncertainTensor(*uncertain_value))
+    else:
+        values = torch.from_numpy(
+            ZeMASamples(
+                n_samples=1,
+                size_scaler=size_scaler,
+                normalize=True,
+                idx_start=idx_start,
+            ).values
+        )
+        mlp = generic_mlp_module(
+            values.shape[1],
+            _construct_out_features_counts(values.shape[1], out_features, depth),
+        )  # type: ignore[call-arg]
+        for value in values:
+            with profile(with_stack=True) as profiler:  # type: ignore[no-untyped-call]
+                mlp(value)
     return profiler
 
 
