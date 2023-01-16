9901113854b477bc6e69e71c212ea4e82897cead
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2023-01-16 21:19:32 +0100

    refactor(zema_dataset): use zema_emc_annotated and move ZeMA stuff to examples

---
+++ src/pytorch_gum_uncertainty_propagation/examples/zema_dataset.py
@@ -0,0 +1,44 @@
+"""An API for accessing the data in the ZeMA remaining-useful life dataset"""
+
+__all__ = ["convert_zema_std_uncertainties_into_synthetic_full_cov_matrices"]
+
+import torch
+from zema_emc_annotated.dataset import ZeMASamples  # type: ignore[import]
+
+from pytorch_gum_uncertainty_propagation.uncertainties import (
+    cov_matrix_from_std_uncertainties,
+    UncertainTensor,
+)
+
+
+def convert_zema_std_uncertainties_into_synthetic_full_cov_matrices(
+    n_samples: int = 1,
+    size_scaler: int = 1,
+    normalize: bool = True,
+    idx_start: int = 0,
+) -> UncertainTensor:
+    """Prepare the ZeMA data for forward propagations in any PyTorch GUM-enabled network
+
+    The main task is turning the standard uncertainties in the ZeMA dataset [Dorst2021]_
+    synthetically into full covariance matrices only for showcasing
+    :class:`~pytorch_gum_uncertainty_propagation.modules.GUMQuadLU`'s
+    capabilities.
+    """
+    uncertain_array = ZeMASamples(n_samples, size_scaler, normalize, idx_start)
+    uncertain_values = UncertainTensor(
+        values=torch.from_numpy(uncertain_array.values),
+        uncertainties=torch.from_numpy(uncertain_array.uncertainties),
+    )
+    assert uncertain_values.uncertainties is not None
+    result_uncertainties = torch.empty(
+        (
+            len(uncertain_values.uncertainties),
+            uncertain_values.uncertainties.shape[1],
+            uncertain_values.uncertainties.shape[1],
+        )
+    )
+    for sample_idx, sample in enumerate(uncertain_values.uncertainties):
+        result_uncertainties[sample_idx, ...] = cov_matrix_from_std_uncertainties(
+            sample, 0.5, 0.5, 0.5
+        )
+    return UncertainTensor(uncertain_values.values, result_uncertainties)
