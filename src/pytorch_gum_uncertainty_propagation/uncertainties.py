ec046bedcb8e999834d1441d3c806743646c12a3
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2023-01-18 09:34:14 +0100

    refactor(uncertainties): streamline implementation to ensure positive semi-definite covariance

--- src/pytorch_gum_uncertainty_propagation/uncertainties.py
+++ src/pytorch_gum_uncertainty_propagation/uncertainties.py
@@ -15,47 +15,22 @@ class UncertainTensor(NamedTuple):
     uncertainties: Tensor | None = None
 
 
-def cov_matrix_from_std_uncertainties(
-    sigma: Tensor, rho: float, phi: float, nju: float, dimen: int | None = None
-) -> Tensor:
-    r"""Set up a covariance matrix from given standard uncertainties
+def cov_matrix_from_std_uncertainties(sigma: Tensor) -> Tensor:
+    r"""Compute :math:`u(x) u^T(x)` given standard uncertainties:math:`u(x)`
 
-    The matrix has the form :math:`V_{ij} = \sigma_i \sigma_j \rho^{\phi * i^\nu}`.
-
-    `V = cov_matrix_from_std_uncertainties(sigma, rho, phi, nu, dimen)`
-    constructs a covariance matrix as specified in [Dierl2016]_ with the column
-    dimension of :math:`\sigma`. In case the dimension of :math:`\sigma` does not
-    match `dimen`, its elements are either cut off or repeated until the desired
-    length is reached. The main idea is to assume higher correlation of values for
-    closer adjacency.
+    The resulting matrix will always be positive semi-definite.
 
     Parameters
     ----------
     sigma : Tensor of shape 1xn
-        standard deviation
-    rho : float
-        scale how many adjacent elements of the covariant structure are related
-    phi : float
-        scale how many adjacent elements of the covariant structure are related
-    nju : float
-        scale hwo many adjacent elements of the covariant structure are related
-    dimen : float
-        number of rows and columns of the covariance matrix
+        standard deviations
 
     Returns
     -------
     Tensor of shape n x n
         covariance matrix
-
-    copyright PTB 2022, B. Ludwig, M. Dierl, D. Oberländer
     """
-    sigma, dimen = _match_dimen_and_std_uncertainty_vec_len(sigma, dimen)
-    corr_factors = torch.eye(dimen)
-    for row in range(1, dimen):
-        torch_diagonal = torch.tensor(rho ** (phi * row**nju)).expand(dimen - row)
-        corr_factors += torch.diag_embed(torch_diagonal, offset=row)
-        corr_factors += torch.diag_embed(torch_diagonal, offset=-row)
-    cov_tensor = torch.mul(sigma, sigma.reshape(-1, 1)) * corr_factors
+    cov_tensor = sigma * sigma.unsqueeze(1)
     assert _is_symmetric(cov_tensor)
     assert _is_positive_semi_definite(cov_tensor)
     return cov_tensor
@@ -74,24 +49,6 @@ def _is_positive_semi_definite(tensor_under_test: Tensor) -> Tensor:
     return torch.all(
         torch.logical_or(
             eigenvalues >= 0,
-            torch.isclose(eigenvalues, tensor_under_test.new_zeros(1), atol=1e-7),
+            torch.isclose(eigenvalues, tensor_under_test.new_zeros(1), atol=1e-6),
         )
     )
-
-
-def _match_dimen_and_std_uncertainty_vec_len(
-    std_uncertainty: Tensor, desired_length: int | None = None
-) -> tuple[Tensor, int]:
-    """Adapt dimension specifier or vector of standard uncertainties
-
-    copyright PTB 2022, B. Ludwig, M. Dierl, D. Oberländer
-    """
-    if desired_length is None:
-        desired_length = 1 if std_uncertainty.dim() == 0 else len(std_uncertainty)
-    else:
-        if desired_length > len(std_uncertainty):
-            std_uncertainty = torch.repeat_interleave(
-                std_uncertainty, int(desired_length / len(std_uncertainty))
-            )
-        std_uncertainty = std_uncertainty[:desired_length]
-    return std_uncertainty, desired_length
