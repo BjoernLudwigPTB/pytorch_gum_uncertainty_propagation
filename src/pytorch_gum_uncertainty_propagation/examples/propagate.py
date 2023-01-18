1986fdef505720008257b86ac3dd37c556129c41
Bjoern Ludwig <bjoern.ludwig@ptb.de>
2023-01-18 14:31:39 +0100

    refactor(propagate): introduce possibility to compute without uncertainties right after with them

--- src/pytorch_gum_uncertainty_propagation/examples/propagate.py
+++ src/pytorch_gum_uncertainty_propagation/examples/propagate.py
@@ -3,13 +3,14 @@
 __all__ = ["assemble_pipeline"]
 
 import datetime
+from itertools import repeat
 from math import ceil
-from typing import Any, Type
+from typing import Any, Iterable, Type
 
 import torch
+from torch import Tensor
 from torch.autograd.profiler import profile
 from torch.nn import Module
-from zema_emc_annotated.dataset import ZeMASamples  # type: ignore[import]
 
 from pytorch_gum_uncertainty_propagation.examples.zema_dataset import (
     convert_zema_std_uncertainties_into_synthetic_full_cov_matrices,
@@ -28,41 +29,35 @@ def assemble_pipeline(
     generic_mlp_module: Type[Module],
     size_scaler: int = 1,
     idx_start: int = 0,
-    out_features: int = 2,
     depth: int = 1,
+    set_uncertainties_to_none: bool = False,
 ) -> Any:
     """Propagate data through an MLP equipped with GUMQuadLU activation"""
     torch.set_default_dtype(torch.double)  # type: ignore[no-untyped-call]
-    if "GUM" in generic_mlp_module.__name__:
-        input_values = convert_zema_std_uncertainties_into_synthetic_full_cov_matrices(
-            n_samples=1, size_scaler=size_scaler, normalize=True, idx_start=idx_start
-        )
-        assert input_values.uncertainties is not None
-        mlp = generic_mlp_module(
-            input_values.values.shape[1],
-            _construct_out_features_counts(
-                input_values.values.shape[1], out_features, depth
-            ),
-        )  # type: ignore[call-arg]
-        for uncertain_value in zip(input_values.values, input_values.uncertainties):
-            with profile(with_stack=True) as profiler:  # type: ignore[no-untyped-call]
-                mlp(UncertainTensor(*uncertain_value))
+    input_values = convert_zema_std_uncertainties_into_synthetic_full_cov_matrices(
+        n_samples=1, size_scaler=size_scaler, normalize=True, idx_start=idx_start
+    )
+    assert input_values.uncertainties is not None
+    if set_uncertainties_to_none:
+        uncertainties: Tensor | Iterable[None] = repeat(None)
     else:
-        values = torch.from_numpy(
-            ZeMASamples(
-                n_samples=1,
-                size_scaler=size_scaler,
-                normalize=True,
-                idx_start=idx_start,
-            ).values
-        )
-        mlp = generic_mlp_module(
-            values.shape[1],
-            _construct_out_features_counts(values.shape[1], out_features, depth),
-        )  # type: ignore[call-arg]
-        for value in values:
-            with profile(with_stack=True) as profiler:  # type: ignore[no-untyped-call]
-                mlp(value)
+        uncertainties = input_values.uncertainties
+    if size_scaler == 1:
+        out_features = 11 - depth
+    elif (max_outputs := size_scaler * 11 - depth) >= 100:
+        out_features = 100
+    else:
+        out_features = max_outputs
+    assert 11 - depth <= out_features <= 100
+    mlp = generic_mlp_module(
+        input_values.values.shape[1],
+        _construct_out_features_counts(
+            input_values.values.shape[1], out_features, depth
+        ),
+    )  # type: ignore[call-arg]
+    for uncertain_value in zip(input_values.values, uncertainties):
+        with profile(with_stack=True) as profiler:  # type: ignore[no-untyped-call]
+            mlp(UncertainTensor(*uncertain_value))
     return profiler
 
 
@@ -87,36 +82,34 @@ if __name__ == "__main__":
     for MLPModule in (GUMSoftplusMLP, GUMQuadLUMLP, GUMSigmoidMLP):
         for layers_additional_to_input in (1, 3, 5, 8):
             for samples_per_sensor in (1, 10, 100, 1000, 2000):
-                with open("timings.txt", "a", encoding="utf-8") as timings_file:
-                    timings_file.write(
-                        f"\n========================================================="
-                        f"==================================================\n"
-                        f"Timing {MLPModule.__name__} for {samples_per_sensor * 11} "
-                        f"inputs and "
-                        f"{layers_additional_to_input} "
-                        f"{'layers' if layers_additional_to_input > 1 else 'layer'}"
-                        f"\n========================================================="
-                        f"==================================================\n"
-                    )
-                profiles = assemble_pipeline(
-                    MLPModule,
-                    samples_per_sensor,
-                    out_features=(
-                        11 - layers_additional_to_input
-                        if samples_per_sensor == 1
-                        else 100
-                    ),
-                    depth=layers_additional_to_input,
-                )
-                with open("timings.txt", "a", encoding="utf-8") as timings_file:
-                    timings_file.write(
-                        profiles.key_averages(group_by_stack_n=2).table(
-                            sort_by="cpu_time_total", row_limit=15
+                for set_to_none in (False, True):
+                    with open("timings.txt", "a", encoding="utf-8") as timings_file:
+                        timings_file.write(
+                            f"\n======================================================="
+                            f"====================================================\n"
+                            f"Timing {MLPModule.__name__} for {samples_per_sensor * 11}"
+                            f" inputs "
+                            f"{'without' if set_to_none else 'with'} "
+                            f"uncertainties and "
+                            f"{layers_additional_to_input} "
+                            f"{'layers' if layers_additional_to_input > 1 else 'layer'}"
+                            f"\n======================================================="
+                            f"====================================================\n"
                         )
+                    profiles = assemble_pipeline(
+                        MLPModule,
+                        samples_per_sensor,
+                        depth=layers_additional_to_input,
                     )
-                profiles.export_chrome_trace(
-                    path=f""
-                    f"{datetime.datetime.now().isoformat('_','hours')}_"
-                    f"{MLPModule.__name__}_{samples_per_sensor * 11}_inputs_"
-                    f"{layers_additional_to_input}_layers_trace.json"
-                )
+                    with open("timings.txt", "a", encoding="utf-8") as timings_file:
+                        timings_file.write(
+                            profiles.key_averages(group_by_stack_n=2).table(
+                                sort_by="cpu_time_total", row_limit=15
+                            )
+                        )
+                    profiles.export_chrome_trace(
+                        path=f""
+                        f"{datetime.datetime.now().isoformat('_','hours')}_"
+                        f"{MLPModule.__name__}_{samples_per_sensor * 11}_inputs_"
+                        f"{layers_additional_to_input}_layers_trace.json"
+                    )
